bdb4666b2df38587d9557a95adca602a132da67a
esiammd <esiammd@hotmail.com>
2025-01-11 20:27:13 +0100

    Fin de sección 7

---
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-home-page',
+  standalone: false,
+
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent {
+  constructor(private gisfService: GifsService) {
+
+  }
+
+  get gifs(): Gif[] {
+    return this.gisfService.gifsList;
+  }
+}
