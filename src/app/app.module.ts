90c5e0467bbedd93e0fce783f38732d87b92a48d
esiammd <esiammd@hotmail.com>
2025-01-24 18:02:27 +0100

    Fin de sección 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { provideHttpClient } from '@angular/common/http';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     GifsModule,
     SharedModule
   ],
